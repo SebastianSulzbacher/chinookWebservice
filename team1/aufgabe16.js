c28c2c3e033fdd29a284950882f01dcceae95c4b
Böschen <martin.boeschen@btc-ag.com>
2018-01-10 13:38:03 +0100

    aufgaben team1

---
+++ team1/aufgabe16.js
@@ -0,0 +1,18 @@
+var fixer = require('./our_fixer.js');
+module.exports = function (app, db) {
+
+    app.get('/invoices/:id', function (request, response) {
+        fixer.get(function (err, res, body) {
+            // var Euro = fixer.convert("USD", "EUR", 1); // 1 US Dollars in Euro
+            var invoices = [];
+            db.get("select * from Invoice where invoiceid = " + request.params.id,
+            function(err, row){
+                row.Total =  fixer.convert("USD", "EUR", row.Total) +" Euro"; 
+                invoices.push(row);
+                response.send(invoices);
+            })
+          });
+
+        
+    });
+}
---
+++ team1/our_fixer.js
@@ -0,0 +1,81 @@
+const http = require('http');
+
+var fixer = new Object( );
+
+var rates = {};
+var triggers = {};
+
+function init() {
+    http.get('http://api.fixer.io/latest', res => {
+        var body = '';
+
+        res.on('data', chunk => {
+            body += chunk;
+        });
+
+        res.on('end', () => {
+            var response = JSON.parse(body);
+            var base = response.base;
+            rates = response.rates;
+            rates[base] = 1;
+            
+            fire('ready');
+        });
+    }).on('error', e => {
+            fire('error', e);
+    });
+}
+
+function allowedRates() {
+    return Object.keys(rates);
+}
+function convert(fromCurrency, toCurrency, amount) {
+    return ((amount / rates[fromCurrency]).toFixed(4) * rates[toCurrency]).toFixed(4);
+}
+
+function on (event, callback) {
+    if (!triggers[event]) {
+        triggers[event] = [];
+    }
+    triggers[event].push(callback);
+}
+function fire (event, params) {
+    var i;
+    if (triggers[event]) {
+        for (i in triggers[event]) {
+            triggers[event][i].apply(null, Array.prototype.slice.call(arguments, 1));
+        }
+    }
+}
+
+fixer.get = function(callback) {
+    http.get('http://api.fixer.io/latest', res => {
+        var body = '';
+
+        res.on('data', chunk => {
+            body += chunk;
+        });
+
+        res.on('end', () => {
+            var response = JSON.parse(body);
+            var base = response.base;
+            rates = response.rates;
+            rates[base] = 1;
+            callback();
+            
+        });
+    });
+};
+
+fixer.convert = function(fromCurrency, toCurrency, amount) {
+    return ((amount / rates[fromCurrency]).toFixed(4) * rates[toCurrency]).toFixed(4);
+}
+
+module.exports = fixer;
+
+// function () {
+//     init();
+//     this.on = on;
+//     this.allowedRates = allowedRates;
+//     this.convert = convert;
+// }
